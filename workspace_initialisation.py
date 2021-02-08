c34742b6508b19e5123b64caea2c334d59e119e0
ewanpugh <67106867+ewanpugh@users.noreply.github.com>
2021-02-08 12:47:22 +0000

    Removed change dir to parent folder

--- workspace_initialisation.py
+++ workspace_initialisation.py
@@ -7,12 +7,10 @@ print('###################### Workspace Initialisation ######################')
 print('######################################################################')
 print('')
 
-# Set working directory as parent of 'Working' folder
-os.chdir('..')
-
 folders_to_create = ['Archive', 'Plots', 'ULG Log Files']
 
 for folder in folders_to_create:
     if folder not in os.listdir():
         os.mkdir(folder)
         print(f'Created {folder} folder')
+print('')
